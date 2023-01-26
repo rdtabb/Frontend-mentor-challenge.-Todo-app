a44f8afa34292bb028a65a3bcb3591e59a59de76
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-01-26 15:14:01 +0300

    now we can add todo-s correctly, for no apparent reason inner text is not showing up, gotta work on it

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const submitButton = document.getElementById('add-task')
 const todoListContainer = document.getElementById('todo-list-container')
-
-const inputText = document.getElementById('input-task')
-const checkbox = document.getElementById('complete-task')
-const checkboxImage = document.getElementById('checkbox__image')
-
-const inputValue = inputText.value
+const addTodoField = document.getElementById('input-value')
+const inputValue = addTodoField.value
 
 submitButton.addEventListener('click', () => {
     const todoContainer = document.createElement('div')
     todoContainer.classList.add('todo-container')
     todoListContainer.appendChild(todoContainer)
 
-    todoContainer.appendChild(inputText)
-    todoContainer.appendChild(checkbox)
-    todoContainer.appendChild(checkboxImage)
+    const todoComplete = document.createElement('input')
+    todoComplete.setAttribute('type', 'checkbox')
+    todoComplete.setAttribute('name', 'check')
+    todoComplete.setAttribute('id', 'complete-task')
 
-    inputText.innerText = inputValue
+    const checkboxImage = document.createElement('img')
+    checkboxImage.classList.add('checkbox__image')
+    checkboxImage.setAttribute('id', 'checkbox__image')
+    checkboxImage.setAttribute('src', './images/icon-check.svg')
+
+    const todoText = document.createElement('input')
+    todoText.setAttribute('id', 'input-task')
+    todoText.setAttribute('type', 'text')
+    todoText.innerText = inputValue //why doesn't it work??? will solve later
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('cross-icon')
+
+    todoContainer.appendChild(todoComplete)
+    todoContainer.appendChild(checkboxImage)
+    todoContainer.appendChild(todoText)
+    todoContainer.appendChild(deleteButton)
 })
