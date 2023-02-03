9f5a7b62c020162135203f758a3f670fced3ea9e
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-02-03 21:47:15 +0300

    well, now it is working way better than it used to. Still cannot make some things work, code is obese and impossible to refactor, maybe I need to rewrite the whole thing

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const showCompleted = document.getElementById("show-completed-b");
 const counter = document.getElementById("counter");
 
 let localStorageCounter = 0;
+
 /* ----------------------------- */
 /* Submit button                 */
 /* ----------------------------- */
@@ -237,11 +238,13 @@ submitButton.addEventListener("click", () => {
   addTodoField.value = "";
 });
 
+//congrats now my code is totally unreadable. I feel smart and dumb at the same time
+
 const numbLocal = parseInt(localStorage.getItem("numb"));
 
 if (numbLocal != null) {
   for (let i = 1; i <= numbLocal; i++) {
-    const innerTodoText = localStorage.getItem(`todo${i}`)
+    const innerTodoText = localStorage.getItem(`todo${i}`);
 
     const todoContainer = document.createElement("div");
     todoContainer.classList.add("todo-container");
@@ -275,7 +278,190 @@ if (numbLocal != null) {
     todoContainer.appendChild(checkboxImage);
     todoContainer.appendChild(todoText);
     todoContainer.appendChild(deleteButton);
+
+    /* ----------------------------- */
+    /* Functions                     */
+    /* ----------------------------- */
+    const setAsCompleted = () => {
+      todoText.classList.add("line-through");
+      todoComplete.setAttribute("data-visible", "false");
+      todoContainer.setAttribute("id", "todo-container-completed");
+    };
+
+    const setAsNonCompleted = () => {
+      todoText.classList.remove("line-through");
+      todoComplete.setAttribute("data-visible", "true");
+      todoContainer.setAttribute("id", "todo-container-active");
+    };
+
+    const pressedDeleteButton = () => {
+      todoListContainer.removeChild(todoContainer);
+      const todoListContainerNumber = todoListContainer.childElementCount;
+      counter.innerText = `${todoListContainerNumber}`;
+      localStorage.setItem("numb", `${todoListContainerNumber}`);
+      localStorage.removeItem(`todo${i}`);
+    };
+
+    const pressedDeleteCompleted = () => {
+      const completedTasks = todoContainer.querySelector(".line-through");
+      const mainContainer = todoContainer.parentNode;
+      if (todoContainer.contains(completedTasks)) {
+        mainContainer.removeChild(todoContainer);
+        const mainContainerNumb = mainContainer.childElementCount;
+        counter.innerText = `${mainContainerNumb}`;
+      }
+    };
+
+    const showActiveFilter = () => {
+      const completedTodo = document.querySelectorAll(
+        "#todo-container-completed"
+      );
+      const activeTodo = document.querySelectorAll("#todo-container-active");
+      completedTodo.forEach((todo) => todo.classList.add("removed"));
+      activeTodo.forEach((nodo) => nodo.classList.remove("removed"));
+
+      showAll.setAttribute("data-active", "false");
+      showCompleted.setAttribute("data-active", "false");
+      showActive.setAttribute("data-active", "true");
+
+      const number = activeTodo.length;
+      counter.innerText = `${number}`;
+    };
+
+    const showCompletedFilter = () => {
+      const completedTodo = document.querySelectorAll(
+        "#todo-container-completed"
+      );
+      const activeTodo = document.querySelectorAll("#todo-container-active");
+      completedTodo.forEach((todo) => todo.classList.remove("removed"));
+      activeTodo.forEach((nodo) => nodo.classList.add("removed"));
+
+      showAll.setAttribute("data-active", "false");
+      showCompleted.setAttribute("data-active", "true");
+      showActive.setAttribute("data-active", "false");
+
+      const number = completedTodo.length;
+      counter.innerText = `${number}`;
+    };
+
+    const showAllFilter = () => {
+      const completedTodo = document.querySelectorAll(
+        "#todo-container-completed"
+      );
+      const activeTodo = document.querySelectorAll("#todo-container-active");
+      completedTodo.forEach((todo) => todo.classList.remove("removed"));
+      activeTodo.forEach((nodo) => nodo.classList.remove("removed"));
+
+      showAll.setAttribute("data-active", "true");
+      showCompleted.setAttribute("data-active", "false");
+      showActive.setAttribute("data-active", "false");
+
+      const mainContainer = todoContainer.parentNode;
+      const mainContainerNumb = mainContainer.childElementCount;
+      counter.innerText = `${mainContainerNumb}`;
+    };
+
+    /* ----------------------------- */
+    /* Mark todo as completed        */
+    /* ----------------------------- */
+    todoComplete.addEventListener("click", () => {
+      const visibility = todoComplete.getAttribute("data-visible");
+      if (visibility === "true") {
+        setAsCompleted();
+      } else {
+        setAsNonCompleted();
+      }
+    });
+
+    /* ----------------------------- */
+    /* Delete with cross icon        */
+    /* ----------------------------- */
+    deleteButton.addEventListener("click", () => {
+      pressedDeleteButton();
+    });
+
+    /* ----------------------------- */
+    /* Delete completed              */
+    /* ----------------------------- */
+    deleteCompletedButton.addEventListener("click", () => {
+      pressedDeleteCompleted();
+    });
+
+    counter.innerText = `${numb}`;
+
+    /* ----------------------------- */
+    /* Show active filter            */
+    /* ----------------------------- */
+    showActive.addEventListener("click", () => {
+      showActiveFilter();
+    });
+
+    /* ----------------------------- */
+    /* Show completed filter         */
+    /* ----------------------------- */
+    showCompleted.addEventListener("click", () => {
+      showCompletedFilter();
+    });
+
+    /* ----------------------------- */
+    /* Show all filter               */
+    /* ----------------------------- */
+    showAll.addEventListener("click", () => {
+      showAllFilter();
+    });
+
+    /* ----------------------------- */
+    /* Drag-n-drop                   */
+    /* ----------------------------- */
+    const draggables = document.querySelectorAll(".draggable");
+    const containers = document.querySelectorAll(".container");
+
+    draggables.forEach((draggable) => {
+      draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+      });
+
+      draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+      });
+    });
+
+    containers.forEach((container) => {
+      container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const draggable = document.querySelector(".dragging");
+        if (afterElement == null) {
+          container.appendChild(draggable);
+        } else {
+          container.insertBefore(draggable, afterElement);
+        }
+      });
+    });
+
+    function getDragAfterElement(container, y) {
+      const draggableElements = [
+        ...container.querySelectorAll(".draggable:not(dragging)"),
+      ];
+
+      return draggableElements.reduce(
+        (closest, child) => {
+          const box = child.getBoundingClientRect();
+          const offset = y - box.top - box.height / 2;
+          if (offset < 0 && offset > closest.offset) {
+            s;
+            return {
+              offset: offset,
+              element: child,
+            };
+          } else {
+            return closest;
+          }
+        },
+        { offset: Number.NEGATIVE_INFINITY }
+      ).element;
+    }
   }
 } else {
-  console.log("you are fucked");
+  console.log(null);
 }
